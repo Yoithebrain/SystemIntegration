752882f4feeb2a5d4a851cadbe4305ff93f9ab7a
tropax91 <tropax6@gmail.com>
2020-01-17 13:05:11 +0100

    The client side

---
+++ client.py
@@ -0,0 +1,29 @@
+import requests
+import jwt
+import json
+
+easyID_url = 'http://localhost:80/login'
+bank_url = 'http://localhost:8081/login'
+skat_url = 'http://localhost:8082/login'
+
+email = input("Enter email: ")
+password = input("Enter password: ")
+payload = {'email': email, 'password': password}
+
+# EASYID REQUEST
+easyID_token = requests.get(easyID_url, params=payload)
+
+if easyID_token.status_code != 404:
+    print('Token: ' + easyID_token.text)
+    print()
+    token_json = { 'token': easyID_token.text }
+
+    # BANK REQUEST
+    bank_balance = requests.get(bank_url, params=token_json)    
+    print('Balance: ' + bank_balance.text + "DKK")
+
+    # SKAT REQUEST
+    skat_debt = requests.get(skat_url, params=token_json)
+    print('Debt: ' + skat_debt.text + "DKK")
+else:
+    print("404 ERROR")
